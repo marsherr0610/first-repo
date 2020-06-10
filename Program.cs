33e2bf52521baaeb12048c11defd7562de88bd9b
marsherr0610 <marsherr0610@gmail.com>
2020-06-10 09:45:21 +0700

    First commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace specialNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a=int.Parse(Console.ReadLine());
+
+            for (a = 1; a <= 20; a++)
+            {
+                if((a / 10 + a % 10 )== 5 || (a / 10 + a % 10) == 7 || (a / 10 + a % 10) == 11)
+                Console.WriteLine(a);   
+            }
+        }
+    }
+}
